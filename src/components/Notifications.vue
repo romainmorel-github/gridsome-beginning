bf671849de6f7e2318d3bb1066de69afbff9dfe6
romainmorel-github <75209623+romainmorel-github@users.noreply.github.com>
2020-12-15 16:58:03 +0100

    Create Notifications.vue

---
+++ src/components/Notifications.vue
@@ -0,0 +1,27 @@
+<div class="notification" v-show="isActive">
+      Ceci est une notification.
+
+        <button class="close" v-on:click="isActive = false">
+          x
+        </button>
+      </div>
+
+<script>
+export default {
+data () {
+  return {
+    isActive : true
+  }
+ }
+}
+<script>
+
+
+<style>
+	.notification {
+	  display:flex;
+	  justify-content: space-between;
+	  background-color:red;
+	  padding: 1.5rem;
+	}
+</style>
